6c72b6d47b1fa9c754bd3a3024f1666849b0a1bf
Eric Meas <emmeas@gmail.com>
2019-07-16 20:55:40 -0700

    Update coliseum_bart_ac_transit_ping.py
    added imports

--- coliseum_bart_ac_transit_ping.py
+++ coliseum_bart_ac_transit_ping.py
@@ -3,3 +3,8 @@ coliseum_bart_ac_transit_ping.py:
 Scraping AC Transit API using Python to ping 
 bus location at Coliseum Bart in Oakland
 """
+
+import webbrowser
+
+import requests
+from requests.exceptions import HTTPError

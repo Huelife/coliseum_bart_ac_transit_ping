ca615ad3fe124a3a39fba50aa0c39bdc703b37ea
Eric Meas <emmeas@gmail.com>
2019-07-21 11:33:09 -0700

    Update coliseum_bart_ac_transit_ping.py
    added variables for request to get data

--- coliseum_bart_ac_transit_ping.py
+++ coliseum_bart_ac_transit_ping.py
@@ -9,3 +9,11 @@ import webbrowser
 import requests
 from requests.exceptions import HTTPError
 from requests import get
+
+api_key = input("API Key: ")
+url = ""
+
+r = requests.get(url)
+data = r.raw.read()
+
+print(data)
